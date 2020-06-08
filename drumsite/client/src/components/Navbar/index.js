66472a00975320670a332f010f5302918b833656
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-08 15:27:57 -0400

    added switch and router to master.js added links to navbar

--- drumsite/client/src/components/Navbar/index.js
+++ drumsite/client/src/components/Navbar/index.js
@@ -1,38 +1,27 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import Home from "../../pages/Home";
+import {Link} from "react-router-dom";
 
 
 export default function Navbar(){
 
 
     return (
-        <Router>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Logo Here</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Logo Here</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/home">Home</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <Switch>
-                <Route exact path={"/", "/home"}>
-                    <Home />
-                </Route>
-                <Route path="*">
-                    <h1>NO FIND!</h1>
-                </Route>
-
-
-            </Switch>
-        </Router>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/home">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/products">Products</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
--- drumsite/client/src/pages/Master.js
+++ drumsite/client/src/pages/Master.js
@@ -1,6 +1,10 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
 import Navbar from "../components/Navbar";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+
+
 
 
 export default function Master(){
@@ -8,8 +12,20 @@ export default function Master(){
 
     return (
         <div>
-            <Navbar />
- 
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path={["/", "/home"]}>
+                        <Home />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                    <Route path="*">
+                        <h1>NO FIND!</h1>
+                    </Route>
+                </Switch>               
+            </Router>
         </div>
     );
 }
