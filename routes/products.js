17a2ea9be217ac17ad2e0898e089ef7f5de44f42
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-17 18:49:32 -0400

    added a find all for products

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
+const Products = require("../models/products");
 
 module.exports = (app) => {
 
     app.get("/products", (req, res) => {
-
+        Products.find({}).then(data => {
+            res.json(data);
+        });
     });
 
 
