01554913d96d4cb02e26b0643cef08a21bb5bdc3
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-24 17:45:46 -0400

    added checkout button

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -8,11 +8,20 @@ export default function Cart(){
 
     return (
         <div>
-            {
-                cartItems.length > 0 ? cartItems.map(e => {
-                    return <CartItem key={e.id} id={e.id} title={e.title} price={e.price} image={e.image} quantity={e.quantity} />
-                }) : <h1>Cart Empty!</h1>
-            }
+            <div className="row">
+                <div className="col-10">
+                    {
+                        cartItems.length > 0 ? cartItems.map(e => {
+                            return <CartItem key={e.id} id={e.id} title={e.title} price={e.price} image={e.image} quantity={e.quantity} />
+                        }) : <h1>Cart Empty!</h1>
+                    }
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2">
+                    <button className="btn btn-primary" type="button">Checkout</button>
+                </div>
+            </div>
         </div>
     );
 }
