2f56e19dfd9558c751548b5642dd936c9a19f8a9
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-24 17:28:33 -0400

    built out cart item display more

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 
 export default function CartItem(props){
 
+    const { id, title, price, image, quantity } = props;
+
     return (
-        <div className="row">
-            <div className="col-2">
-                <div className="cart-image"></div>
+        <div className="row" id={id}>
+            <div className="col-1"></div>
+            <div className="col-10 cart-style">
+                <div className="cart-image" style={{ backgroundImage: `url(${image})` }}/>
+                <div className="cart-title">{title}</div>
+                <div className="cart-quantity">Quantity: {quantity}</div>
+                <div className="cart-price">${price}</div>
             </div>
+            <div className="col-1"></div>
         </div>
     );
 }
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,12 +1,18 @@
 import React from "react";
-
-
+import CartItem from "../components/CartItem";
+import GlobalContext from "../utils/GlobalContext";
 
 export default function Cart(){
 
+    const { cartItems } = React.useContext(GlobalContext);
+
     return (
         <div>
-            
+            {
+                cartItems.length > 0 ? cartItems.map(e => {
+                    return <CartItem key={e.id} id={e.id} title={e.title} price={e.price} image={e.image} quantity={e.quantity} />
+                }) : <h1>Cart Empty!</h1>
+            }
         </div>
     );
 }
