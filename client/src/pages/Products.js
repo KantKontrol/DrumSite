d244a7fb18719bc0e019039e01de06366cec1186
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-24 10:23:29 -0400

    added get products to products page

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 export default function Products(){
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        let rendered = true;
+
+        if(rendered){
+            getProducts();
+        }
+
+        return rendered = false;
+    });
+
+    function getProducts(){
+        API.getAllProducts().then(res => {
+            setProducts(res.data);
+        });
+    }
 
     return (
         <div>
