930f7fd2ad506860cd4101c77ef69a0b73275234
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-26 17:45:39 -0400

    reorganized cart items

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -12,11 +12,32 @@ export default function CartItem(props){
         <div className="row" id={id}>
             <div className="col-1"></div>
             <div className="col-10 cart-style">
-                <div className="cart-image" style={{ backgroundImage: `url(${image})` }}/>
-                <div className="cart-title">{title}</div>
-                <div className="cart-quantity">Quantity: {quantity}</div>
-                <div className="cart-price">${price}</div>
-                <div className="remove-button" onClick={() => { removeFromCart(id) }}><i class="fa fa-close" style={{fontSize: 32 + "px", color: "red"}}></i></div>
+                <div className="row">
+                    <div className="col-2">
+                        <div className="cart-image" style={{ backgroundImage: `url(${image})` }}/>
+                    </div>
+                    <div className="col-6">
+                        <div className="cart-title">{title}</div>
+
+                        <div className="cart-price">${price}</div>
+
+                    </div>
+                    <div className="col-4">
+                    <div style={{ fontSize: "medium", textAlign: "left"}}>Quantity</div>
+                    <div className="input-group" >
+                        
+                            <div className="input-group-prepend">
+                                <button className="btn btn-secondary" type="button" id="button-addon1"><i className="fas fa-arrow-down"></i></button>
+                            </div>
+                            <div className="cart-quantity">{quantity}</div>
+                            <div className="input-group-append">
+                                <button className="btn btn-secondary" type="button" id="button-addon2"><i className="fas fa-arrow-up"></i></button>
+                            </div>
+                    </div>
+                        
+                        <div className="remove-button" onClick={() => { removeFromCart(id) }}><i className="fa fa-close" style={{fontSize: 32 + "px", color: "red"}}></i></div>
+                    </div>
+                </div>
             </div>
             <div className="col-1"></div>
         </div>
