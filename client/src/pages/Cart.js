1a4fe719b132c922dc0416d4c320823b95442133
KantKontrol <nick.derissio.tech@gmail.com>
2020-06-26 18:05:51 -0400

    added rudimentary subTotal

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -6,12 +6,15 @@ export default function Cart(){
 
     const { cartItems } = React.useContext(GlobalContext);
 
+    let subTotal = 0;
+
     return (
         <div>
             <div className="row">
                 <div className="col-10" style={{ margin: "0px auto"}}>
                     {
                         cartItems.length > 0 ? cartItems.map(e => {
+                            subTotal+=e.price;
                             return <CartItem key={e.id} id={e.id} title={e.title} price={e.price} image={e.image} quantity={e.quantity} />
                         }) : <h1>Cart Empty!</h1>
                     }
@@ -19,6 +22,7 @@ export default function Cart(){
             </div>
             <div className="row">
                 <div className="col-2">
+                    <h1>Subtotal: {subTotal}</h1>
                     <button className="btn btn-primary" type="button">Checkout</button>
                 </div>
             </div>
