ae18eceae976f0d49675afed2afcd8f9ce1e4ccd
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-13 10:30:03 -0400

    modified cart layout

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -16,13 +16,14 @@ export default function Cart(){
                         cartItems.length > 0 ? cartItems.map(e => {
                             subTotal+=e.price;
                             return <CartItem key={e.id} id={e.id} title={e.title} price={e.price} image={e.image} quantity={e.quantity} />
-                        }) : <h1>Cart Empty!</h1>
+                        }) : <h1 className="mx-auto" style={{ width: "fit-content" }}>Cart Empty!</h1>
                     }
                 </div>
             </div>
             <div className="row">
+                <div className="col-10"></div>
                 <div className="col-2">
-                    <h1>Subtotal: {subTotal}</h1>
+                    <h1>Subtotal: ${subTotal}</h1>
                     <button className="btn btn-primary" type="button">Checkout</button>
                 </div>
             </div>
