43bb159e68e42ba4f15953340b678a1c03abc1a4
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-14 16:54:45 -0400

    made it visible which page the user was on through the navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation } from "react-router-dom";
 //import GlobalContext from "../../utils/GlobalContext";
 import "./index.css";
 
@@ -7,6 +7,7 @@ import "./index.css";
 export default function Navbar(props){
 
     //let c = React.useContext(GlobalContext);
+    let location = useLocation().pathname;
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light  navbar-cus-color">
@@ -18,13 +19,13 @@ export default function Navbar(props){
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <Link className="nav-link text-color" to="/home">Home</Link>
+                        <Link className={ location == "/home" ? "nav-link text-color nav-active" : "nav-link text-color"} to="/home">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link text-color" to="/drums">Drums</Link>
+                        <Link className={ location == "/drums" ? "nav-link text-color nav-active" : "nav-link text-color"} to="/drums">Drums</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link text-color" to="/contact">Contact</Link>
+                        <Link className={ location == "/contact" ? "nav-link text-color nav-active" : "nav-link text-color"} to="/contact">Contact</Link>
                     </li>
                     <li className="nav-item">
                         {/*<Link className="nav-link" to="/cart"><i className="fas fa-shopping-cart"></i><span className="badge badge-primary badge-pill">{c.numInCart}</span></Link>*/}
