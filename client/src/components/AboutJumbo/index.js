520ca0a24c0f06ab0b6a04f01c536394a0c8f913
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-14 16:06:42 -0400

    modified text sizes on jumbotron

--- client/src/components/AboutJumbo/index.js
+++ client/src/components/AboutJumbo/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 export default function AboutJumbo(props){
     return (
         <div className="jumbotron">
-            <h1 className="display-4">About Us!</h1>
+            <h1 className="display-4">{props.title}</h1>
             <p className="lead">{props.lead}</p>
             <hr className="my-4" style={{ backgroundColor: "#ABEBD2"}} />
-            <p>{props.about}</p>
+            <p style={{ fontSize: 1.25 + "rem"}}>{props.about}</p>
             <p className="lead">
-                <a className="btn btn-primary btn-lg" href={props.href} role="button">{props.atext}</a>
+                <a className="btn btn-lg jumbo-button-style" href={props.href} role="button">{props.atext}</a>
             </p>
         </div>
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,15 +5,15 @@ import AboutJumbo from "../components/AboutJumbo";
 
 export default function Home(){
 
-    let aboutLead = "Laborum reprehenderit duis culpa minim minim enim veniam adipisicing ad laborum proident ut laborum sit.";
-    let aboutBody = "Ea laborum est nisi laborum Lorem laborum magna ullamco labore quis ullamco eu. Elit veniam consectetur exercitation anim cupidatat tempor velit pariatur quis. Cillum culpa Lorem fugiat deserunt deserunt aute officia officia mollit occaecat adipisicing laboris excepteur. Labore sint officia pariatur cupidatat quis. Lorem quis officia qui non proident cillum labore dolore.";
+    let aboutLead = "Drum decor made from used bass drums, toms, and snares.";
+    let aboutBody = "Every piece of drum decor we make, is hand crafted to your liking. Drum decor can be made either from the drums we collect, or a drum you provide! All drum decor is crafted with a keen sense of detail, ready to spice a living space, office, or to be a conversation piece.";
 
     return (
         <div className="">
             <Hero text="Welcome!" />
             <div className="container no-padding push-footer">
                 <RandomProducts />
-                <AboutJumbo lead={aboutLead} about={aboutBody} href="/drums" atext="View Drums"/>
+                <AboutJumbo title="About our decor," lead={aboutLead} about={aboutBody} href="/drums" atext="View Drums"/>
             </div>
         </div>
     );
