c63644e9f7c4d7c5f102a4a94ccd38c89f3cf3a6
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-14 16:23:01 -0400

    moved footer text, began to create contact page

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Contact(){
+
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-10">
+
+                </div>
+                <div className="col-1"></div>
+            </div>
+        </div>
+    )
+}
