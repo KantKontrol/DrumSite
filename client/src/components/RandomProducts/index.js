dd0f135e1752e84a0cc3b37cf686ab90fabba673
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-14 14:52:45 -0400

    gave margin to random products header

--- client/src/components/RandomProducts/index.js
+++ client/src/components/RandomProducts/index.js
@@ -27,7 +27,7 @@ export default function RandomProducts(){
     return (
         <div>
             <div className="row">
-                <div className="col-12"><h4 className="title">Recently added...</h4></div>
+                <div className="col-12" style={{ marginTop: 10 + "px" }}><h4 className="title">Recently added...</h4></div>
             </div>
             <div className="row ran-prod-style">
                 { latestProducts.length > 0 ? latestProducts.map(e => {
